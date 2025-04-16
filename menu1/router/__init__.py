686c1e7fb7839929419012d389218e5e1a1772b4
dynames44 <dynames.n44@gmail.com>
2025-04-16 14:19:06 +0900

    Jinja2 탬플릿 샘플

---
+++ menu1/router/__init__.py
@@ -0,0 +1,3 @@
+from .temp1 import router as view1
+
+__all__ = ["view1"] 
---
+++ menu1/router/temp1.py
@@ -0,0 +1,30 @@
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from fastapi.responses import HTMLResponse
+from core.templates import templates
+
+#FastAPI 라우터 인스턴스 생성
+router = APIRouter(prefix="/menu1", include_in_schema=False)  # include_in_schema:  Swagger UI 노출여부
+
+class Item(BaseModel):
+    name: str
+    price: float
+
+# response_class=HTMLResponse를 생략하면 application/json으로 Swagger UI에서 인식(별 문제는 없음)
+@router.get("/items/{id}", response_class=HTMLResponse)
+
+# template engine을 사용할 경우 반드시 Request 객체가 인자로 입력되어야 함. 
+async def read_item(request: Request, id: str, q: str | None = None):
+    
+    # 내부에서 pydantic 객체 생성. 
+    item = Item(name="test_item", price=10)
+    
+    # pydantic model값을 dict 변환. 
+    item_dict = item.model_dump()
+    item_dict["temp"] = "temp01"
+
+    return templates.TemplateResponse(
+        request=request,
+        name="menu1/item.html",
+        context={"id": id, "q_str": q, "item": item, "item_dict": item_dict}
+    )

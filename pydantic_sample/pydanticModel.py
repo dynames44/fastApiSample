093067591b24764ad0eb8044192a052a4e3c2a5b
dynames44 <dynames.n44@gmail.com>
2025-04-17 16:44:00 +0900

    모델객체 별도 구성

--- pydantic_sample/pydanticModel.py
+++ pydantic_sample/pydanticModel.py
@@ -1,7 +1,17 @@
 from pydantic import BaseModel,Field, model_validator
 
+#data class 는 뭔가?
+
 class pydanticBase1(BaseModel):
+    name: str 
+    description: str 
+    price: float 
+    tax: float = None
+    
+class pydanticBase2(BaseModel):
     name: str = Field(..., min_length=2, max_length=50)
     description: str = Field(None, max_length=500)
     price: float = Field(..., ge=0)
-    tax: float = None
+    tax: float = None    
+    
+    
